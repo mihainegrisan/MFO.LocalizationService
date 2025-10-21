5d003e4d5695db945a6bbca3c34d4a2d58327051
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-10-21 10:08:41 +0300

    Create Country and Region entities

---
+++ src/MFO.LocalizationService.Domain/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿namespace MFO.LocalizationService.Domain.Entities;
+
+public class Country
+{
+    public required Guid CountryId { get; set; }
+    public required string Iso2Code { get; set; }
+    public required string Iso3Code { get; set; }
+    public required string Name { get; set; }
+    public required string DefaultCurrencyCode { get; set; }
+
+    // public string? PhoneCode { get; set; }  // "+1", "+33", "+49"
+    // public bool IsActive { get; set; }
+
+    public Guid? DefaultCurrencyId { get; set; }
+    public Currency? DefaultCurrency { get; set; }
+    public ICollection<Region>? Regions { get; set; }
+}
