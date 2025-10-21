40072e3ff834d355976210045b7eca1d0cc4368e
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-10-21 10:22:49 +0300

    Fix #1 add country and region DTOs

---
+++ src/MFO.LocalizationService.Application/DTOs/RegionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MFO.LocalizationService.Application.DTOs;
+
+public class RegionDto
+{
+    public required Guid RegionId { get; set; }
+    public required string Code { get; set; }
+    public required string Name { get; set; }
+
+    public CountryDto? CountryDto { get; set; }
+}
