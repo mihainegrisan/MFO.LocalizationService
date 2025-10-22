a84f6eca50044e690d63ddc9807587c55e9fd0fd
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-10-22 10:39:28 +0300

    Fix #8 Configure column constraints for countries table

---
+++ src/MFO.LocalizationService.Infrastructure/Persistence/Configurations/CountryConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using MFO.LocalizationService.Domain.Common;
+using MFO.LocalizationService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MFO.LocalizationService.Infrastructure.Persistence.Configurations;
+
+public class CountryConfiguration : IEntityTypeConfiguration<Country>
+{
+    public void Configure(EntityTypeBuilder<Country> builder)
+    {
+        builder.HasKey(x => x.CountryId);
+
+        builder.Property(x => x.Iso2Code)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.Iso2CodeLength);
+
+        builder.Property(x => x.Iso3Code)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.Iso3CodeLength);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.CountryNameMaxLength);
+
+        builder.Property(x => x.DefaultCurrencyCode)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.DefaultCurrencyCodeLength);
+
+        // Natural key indices for fast lookup
+        builder.HasIndex(x => x.Iso2Code)
+            .IsUnique()
+            .HasDatabaseName("IX_Country_Iso2Code");
+
+        builder.HasIndex(x => x.Iso3Code)
+            .IsUnique()
+            .HasDatabaseName("IX_Country_Iso3Code");
+
+        // Foreign key to Currency
+        builder.HasOne(x => x.DefaultCurrency)
+            .WithMany()
+            .HasForeignKey(x => x.DefaultCurrencyId)
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
+
+        // One-to-many: Country to Regions
+        builder.HasMany(x => x.Regions)
+            .WithOne(x => x.Country)
+            .HasForeignKey(x => x.CountryId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ src/MFO.LocalizationService.Infrastructure/Persistence/LocalizationContext.cs
@@ -0,0 +1,23 @@
+﻿using MFO.LocalizationService.Domain.Entities;
+using MFO.LocalizationService.Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace MFO.LocalizationService.Infrastructure.Persistence;
+
+public class LocalizationContext : DbContext
+{
+    public LocalizationContext(DbContextOptions<LocalizationContext> options) 
+        : base(options)
+    {
+        
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new CountryConfiguration());
+    }
+}
