af621307a013a83f7571a8e07c337a104ed07957
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-10-31 11:22:06 +0200

    Fix #34 Apply auditing data on save

--- src/MFO.LocalizationService.Infrastructure/Persistence/LocalizationDbContext.cs
+++ src/MFO.LocalizationService.Infrastructure/Persistence/LocalizationDbContext.cs
@@ -1,15 +1,18 @@
-﻿using MFO.LocalizationService.Domain.Entities;
+﻿using MFO.LocalizationService.Application.Interfaces;
+using MFO.LocalizationService.Domain.Entities;
 using MFO.LocalizationService.Infrastructure.Persistence.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace MFO.LocalizationService.Infrastructure.Persistence;
 
-public class LocalizationContext : DbContext
+public class LocalizationDbContext : AuditableDbContextBase
 {
-    public LocalizationContext(DbContextOptions<LocalizationContext> options) 
-        : base(options)
+    public LocalizationDbContext(
+        DbContextOptions<LocalizationDbContext> options,
+        IDateTimeProvider dateTimeProvider,
+        IUserContextProvider userContextProvider)
+        : base(options, dateTimeProvider, userContextProvider)
     {
-        
     }
 
     public DbSet<Country> Countries { get; set; }
@@ -25,5 +28,17 @@ public class LocalizationContext : DbContext
         modelBuilder.ApplyConfiguration(new RegionConfiguration());
         modelBuilder.ApplyConfiguration(new CurrencyConfiguration());
         modelBuilder.ApplyConfiguration(new ExchangeRateConfiguration());
+    }
+
+    public override int SaveChanges()
+    {
+        ApplyAuditing();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        ApplyAuditing();
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
